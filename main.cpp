6b7c3d461fa0472a1171e37a5a65053e65377af2
isaacgreenfield <ivtim708@gmail.com>
2025-10-20 12:37:33 +0300

    1111

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
-#include<cstddef>
+
+int ** create(size_t rows, size_t cols);
+void remove(int ** n, size_t rows, size_t cols);
+
 int main() {
     size_t rows = 0, cols = 0;
     std::cin >> rows >> cols;
     if (!std::cin) {
         return 1;
     }
-    std::cout << rows << " " << cols << std::endl;
+    //std::cout << rows << " " << cols << std::endl;
+    int ** n = create(rows, cols);
+    remove(n, rows, cols);
     return 0;
 }
